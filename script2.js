1631a689ae7cc867b18a9f4304d11a27a1a5d610
Isabel Blomström <isabel.blomstrom@gmail.com>
2019-10-31 15:11:59 +0100

    förbättringar

--- script2.js
+++ script2.js
@@ -28,29 +28,35 @@
     }
 
 }*/
+
+const gameState = {
+  hasFoundKey: false,
+  hasFoundSmallKey: false,
+}
+
 function spelaSpel() {
     alert('Välkommen till The Escape From Madness!')
   
     alert('Du vaknar till ljudet av en nyckel som vrids om. När du sätter dig upp ser du att det i rummet finns en byrå och en växt.');
 
-  
-    
-    let utforska = prompt('Vad vill du utforska, byrån eller växten?').toLowerCase()  
     let decision;
     let decision2;
     let decision3;
     let decision4;
-    let decision5;
-    let decision6;
-    let decision7;
-
+  
     
-    if (utforska === "byrån" || utforska === "byrå") {
-      byrån();
+    let utforska = prompt('Vad vill du utforska, byrån eller växten?').toLowerCase()  
+    
+    
+    
+    if (utforska === "byrån" || utforska === "byrå" && gameState.hasFoundSmallKey) {
+      byrån()
     } else if (utforska === "växten" || utforska === "växt") {
-      växten();
+      hasFoundSmallKey = true
+      växten()
     } else {
-        alert("du dog")
+        alert("Du har ingen nyckel till byrån.")
+        spelaSpel()
     }
   
   
@@ -61,7 +67,8 @@ function spelaSpel() {
       if(decision === "ja") {
         hallway();
       } else {
-        alert("Du är för långsam och vetenskapsmannen hann tillbaka")
+        alert("du dog")
+        
       }
     }
     
@@ -69,12 +76,14 @@ function spelaSpel() {
   
     function växten() {
       alert("Du kikar ner i jorden och ser att där ligger en nyckel.");
-      decision2 = prompt("Vill du öppna dörren med nyckeln?").toLowerCase();
+      decision2 = prompt("Vill du gå till byrån?").toLowerCase();
 
       if(decision2 === "ja") {
-        hallway();
+        byrån()
       } else {
-        alert("Du är för långsam och vetenskapsmannen hann tillbaka")
+        alert("Det var ett dåligt beslut. Du har blivit inlåst av en galen vetenskapsman som \
+har experimenterat på dig och nu kommer han tillbaka.\
+")
       }
     }
   
@@ -87,7 +96,7 @@ function spelaSpel() {
         alert("Ytterdörren är låst och ditt finger kan inte låsa upp den")
         hallway()
       } else if(decision3 === "akvarium" || decision3 === "akvariumet") {
-        decision3 = true
+        gameState.hasFoundKey = true
         alert("Du stoppar ner handen och ser en piraya. Snabbt fiskar du upp den lilla skattkistan, däri ligger en nyckel..")
         hallway()
       } else if(decision3 === "lilla dörren" || decision3 === "liten dörr" || decision3 === "dörren") {
@@ -99,13 +108,18 @@ function spelaSpel() {
       alert("När du kommer in i salongen hittar du en butler fastkedjad vid en stol.")
       decision4 = prompt("Vill du försöka hjälpa butlern att komma loss?").toLowerCase()
 
-      if(decision4 === "ja"/* && decision3 === "akvarium"*/ ) {
+      if(decision4 === "ja" && gameState.hasFoundKey) {
         alert("Du hjälper butlern att komma loss och han är mycket tacksam. Han berättar att en galen\
-        vetenskapsman har experimenterat på dig, men nu ska han hjälpa dig att komma ut. Han tar dig med\
-        tillbaka ut i stora hallen och håller sitt finger på avläsaren. Det klickar till och dörren öppnas.\
-        Du är FRI!")
+ vetenskapsman har experimenterat på dig, men nu ska han hjälpa dig att komma ut. Han tar dig med\
+ tillbaka ut i stora hallen och håller sitt finger på avläsaren. Det klickar till och dörren öppnas.\
+ Du är FRI!")
 
-      } else {
+      } else if(decision4 === "nej") {
+        alert("Plötsligt stormar en galen vetenskapsman in. Han har experimenterat på dig en tid \
+och är väldigt sugen på att fortsätta. Du blir inlåst i rummet igen. Karma.")
+      }
+      
+      else {
         alert("Du har ingen nyckel som passar till låset")
         hallway()
       }
